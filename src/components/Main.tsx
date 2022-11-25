2e6f954b8b59eb4a828717bb22b9306122eb4bcf
kylemcshea <kmcshea@stevens.edu>
2022-11-25 17:26:57 -0500

    fix(modal) make modal scrollable

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Offerings from "./Offerings";
 import Marketing from "./Marketing";
 import TimeLapse from "./TimeLapse";
@@ -7,6 +7,15 @@ import BookingModal from "./BookingModal";
 
 const Main: React.FC = () => {
   const [booking, setBooking] = useState<string>("");
+  useEffect(() => {
+    if (booking !== "") {
+      // do not allow scrolling on Main but allow scrolling on the BookingModal
+      document.documentElement.style.overflow = "hidden";
+    } else {
+      document.documentElement.style.overflow = "scroll";
+    }
+  }, [booking]);
+
   return (
     <div className="mx-6 py-3">
       <div className="slide-left">
