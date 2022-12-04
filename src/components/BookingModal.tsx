e7bd42bd6555760ffc41714b7c84b3711e6830e1
kylemcshea <kmcshea@stevens.edu>
2022-12-03 23:01:44 -0500

    you can now contact through the form

--- src/components/BookingModal.tsx
+++ src/components/BookingModal.tsx
@@ -5,6 +5,8 @@ import DialogTitle from "@mui/material/DialogTitle";
 import Slide from "@mui/material/Slide";
 import { TransitionProps } from "@mui/material/transitions";
 
+import { useForm } from "@formspree/react";
+
 const Transition = React.forwardRef(function Transition(
   props: TransitionProps & {
     children: React.ReactElement<any, any>;
@@ -29,6 +31,9 @@ const BookingModal = ({
     };
     document.addEventListener("keydown", handleEscape);
   }, [onClose]);
+
+  const [state, handleSubmit] = useForm("xwkzgepj");
+
   return (
     <div>
       <Dialog
@@ -41,115 +46,121 @@ const BookingModal = ({
         <DialogTitle>🔨 What can we do for you?</DialogTitle>
         <DialogContent>
           <div className="py-6 px-6 lg:px-8">
-            <form className="space-y-6" action="#">
-              <div>
-                <label
-                  htmlFor="first-name"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
-                >
-                  📝 First Name
-                </label>
-                <input
-                  type="text"
-                  name="first-name"
-                  id="first-name"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                  placeholder="First Name"
-                  required
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="last-name"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
-                >
-                  📝 Last Name
-                </label>
-                <input
-                  type="text"
-                  name="last-name"
-                  id="last-name"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                  placeholder="Last Name"
-                  required
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
-                >
-                  📧 Your email
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                  placeholder="name@company.com"
-                  required
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="phone"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
-                >
-                  📞 Phone #
-                </label>
-                <input
-                  type="phone"
-                  name="phone"
-                  id="phone"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                  placeholder="(555) 555-5555"
-                  required
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="booking"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
-                >
-                  👷‍♀️ Offering(s) of Interest
-                </label>
-                <input
-                  type="text"
-                  name="booking"
-                  id="booking"
-                  defaultValue={booking}
-                  placeholder={booking}
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                  required
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
-                >
-                  ✨ Offering Details
-                </label>
-                <textarea
-                  name="booking-details"
-                  id="booking-details"
-                  placeholder="Put any information here that you think would be helpful for us to know"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                  required
-                />
-              </div>
-              <button
-                type="submit"
-                className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                Submit Request
-              </button>
-              <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-                I, Matthew McShea, will get back to you{" "}
-                <span className="text-green-800 underline">
-                  as soon as possible!
-                </span>
-              </div>
+            <form className="space-y-6" action="#" onSubmit={handleSubmit}>
+              {state?.succeeded ? (
+                <p>🤝 Thank you for booking a quote with us!</p>
+              ) : (
+                <>
+                  <div>
+                    <label
+                      htmlFor="first-name"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
+                    >
+                      📝 First Name
+                    </label>
+                    <input
+                      type="text"
+                      name="first-name"
+                      id="first-name"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                      placeholder="First Name"
+                      required
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="last-name"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
+                    >
+                      📝 Last Name
+                    </label>
+                    <input
+                      type="text"
+                      name="last-name"
+                      id="last-name"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                      placeholder="Last Name"
+                      required
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="email"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
+                    >
+                      📧 Your email
+                    </label>
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                      placeholder="name@company.com"
+                      required
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="phone"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
+                    >
+                      📞 Phone #
+                    </label>
+                    <input
+                      type="phone"
+                      name="phone"
+                      id="phone"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                      placeholder="(555) 555-5555"
+                      required
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="booking"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
+                    >
+                      👷‍♀️ Offering(s) of Interest
+                    </label>
+                    <input
+                      type="text"
+                      name="booking"
+                      id="booking"
+                      defaultValue={booking}
+                      placeholder={booking}
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                      required
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="password"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
+                    >
+                      ✨ Offering Details
+                    </label>
+                    <textarea
+                      name="booking-details"
+                      id="booking-details"
+                      placeholder="Put any information here that you think would be helpful for us to know"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                      required
+                    />
+                  </div>
+                  <button
+                    type="submit"
+                    className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    Submit Request
+                  </button>
+                  <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
+                    I, Matthew McShea, will get back to you{" "}
+                    <span className="text-green-800 underline">
+                      as soon as possible!
+                    </span>
+                  </div>
+                </>
+              )}
             </form>
           </div>
         </DialogContent>
