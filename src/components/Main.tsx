253a942a109999b0acceeea782e55d5dd1dcefb9
kylemcshea <kmcshea@stevens.edu>
2022-12-03 22:38:14 -0500

    carousel enablement

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,17 +4,11 @@ import Marketing from "./Marketing";
 import TimeLapse from "./TimeLapse";
 import Contact from "./Contact";
 import BookingModal from "./BookingModal";
+import Gallery from "./Gallery";
+import { galleryData } from "../data/galleryData";
 
 const Main: React.FC = () => {
   const [booking, setBooking] = useState<string>("");
-  useEffect(() => {
-    if (booking !== "") {
-      // do not allow scrolling on Main but allow scrolling on the BookingModal
-      document.documentElement.style.overflow = "hidden";
-    } else {
-      document.documentElement.style.overflow = "scroll";
-    }
-  }, [booking]);
 
   return (
     <div className="mx-6 py-3">
@@ -25,6 +19,8 @@ const Main: React.FC = () => {
       <div className="mt-3 slide-right">
         <Marketing />
       </div>
+      {/* TODO: Get the images to actually show up. For some reason they are showing up blank. */}
+      <Gallery carouselImages={galleryData()} />
       <BookingModal booking={booking} onClose={() => setBooking("")} />
       <Contact />
     </div>
